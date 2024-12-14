407d65138bce4277565488473c6e12b4d669100d
Your Name <you@example.com>
2024-12-14 16:20:23 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Ensure that the DOM content is fully loaded before running the script
+    var typed = new Typed(".text", {
+        strings: ["Frontend Developer", "YouTube", "Web Developer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
+
